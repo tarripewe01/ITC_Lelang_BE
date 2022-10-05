83bb95e3f2cba957e1d5926fd17b33d0eaec0225
tarripewe <tarripewe01@gmail.com>
2022-10-05 19:30:00 +0700

    CRUD Products: DONE

--- controllers/Produk.js
+++ controllers/Produk.js
@@ -1,17 +1,6 @@
 
 const ProdukModel = require("../models/Produk");
 
-// const createProduct = async (req, res) => {
-//   const newProduk = new ProdukModel(req.body);
-
-//   try {
-//     const savedProduk = await newProduk.save();
-//     res.status(200).json(savedProduk);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// };
-
 const createProduct = async (req, res) => {
 
   const fileName = req.files.map((path) => {
@@ -54,85 +43,22 @@ const getProductById = async (req, res) => {
 
 const updateProduct = async (req, res) => {
   const { id } = req.params;
-  const {
-    cabang,
-    nama_produk,
-    photo_produk,
-    harga,
-    no_lot,
-    kondisi_mesin,
-    kondisi_exterior,
-    kondisi_interior,
-    kategori_produk,
-    merk_produk,
-    model_produk,
-    tahun_produk,
-    transmisi,
-    no_rangka,
-    no_mesin,
-    kapasitas_mesin,
-    odometer,
-    isActive,
-    catatan,
-    no_polisi,
-    warna,
-    stnk,
-    exp_stnk,
-    faktur,
-    ktp,
-    kwitansi,
-    form_A,
-    sph,
-    keur,
-    bpkb,
-    tanggal_mulai,
-    tanggal_selesai,
-    status_lelang,
-  } = req.body;
+
+  const fileName = req.files.map((path) => {
+    return path.filename
+  })
+  let {photo_path, ...details} = req.body
+  photo_path = `/uploads/${fileName}`
+
+  const dat = {...details, photo_path}
 
   try {
-    const formUpdateProduk = {
-      cabang,
-      nama_produk,
-      photo_produk,
-      harga,
-      no_lot,
-      kondisi_mesin,
-      kondisi_exterior,
-      kondisi_interior,
-      kategori_produk,
-      merk_produk,
-      model_produk,
-      tahun_produk,
-      transmisi,
-      no_rangka,
-      no_mesin,
-      kapasitas_mesin,
-      odometer,
-      isActive,
-      catatan,
-      no_polisi,
-      warna,
-      stnk,
-      exp_stnk,
-      faktur,
-      ktp,
-      kwitansi,
-      form_A,
-      sph,
-      keur,
-      bpkb,
-      tanggal_mulai,
-      tanggal_selesai,
-      status_lelang,
-      _id: id,
-    };
-    await ProdukModel.findByIdAndUpdate(id, formUpdateProduk, { new: true });
-    res.json(formUpdateProduk);
+    const product = await ProdukModel.findByIdAndUpdate(id, dat, { new: true });
+    res.status(200).json(product);
   } catch (error) {
     res.status(500).json(error);
   }
-};
+}
 
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
